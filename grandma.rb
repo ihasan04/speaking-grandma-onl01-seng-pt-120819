1eb1e19ff14612c80ae5ea4404153655dd162b6c
Ismael Hasan <ismael.hasan@live.com>
2019-12-11 23:23:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+     return "I LOVE YOU TOO PUMPKIN!"
   elsif != string.upcase
-    return "HUH?! SPEAK UP, SONNY!
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
