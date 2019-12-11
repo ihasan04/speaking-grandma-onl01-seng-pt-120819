30d4a8d8fbe389a3b4a73b5b35c4f4221059f2ca
Ismael Hasan <ismael.hasan@live.com>
2019-12-11 23:30:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(string)
   elsif string == string.upcase
      return "NO, NOT SINCE 1938!"
      
-  elsif != string.upcase
+  else
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
